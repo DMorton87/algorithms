5e3e1840c4155d505e1ef1870636d30f8a7b8fd4
DMorton87 <transpective@gmail.com>
2023-10-14 21:30:37 +0800

    Added merge_sort code and explaination

---
+++ merge_sort/merge_sort.py
@@ -0,0 +1,30 @@
+def merge_sort(arr):
+    if len(arr) < 2:
+        return arr
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])  
+    return merge(left, right)
+    
+
+def merge(left, right):
+    result = []
+    i = j = 0
+    
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])  
+            j += 1
+    
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+        
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+    
+    return result
